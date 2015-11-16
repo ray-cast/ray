ee788d4dc210d896f62d91f7287319e6e9991549
ray-cast <2221870259@qq.com>
2015-11-17 03:13:17 +0800

    添加了GameObjectMananger类管理所有GameObject

---
+++ include/ray/game_object_manager.h
@@ -0,0 +1,79 @@
+// +----------------------------------------------------------------------
+// | Project : ray.
+// | All rights reserved.
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2015.
+// +----------------------------------------------------------------------
+// | * Redistribution and use of this software in source and binary forms,
+// |   with or without modification, are permitted provided that the following
+// |   conditions are met:
+// |
+// | * Redistributions of source code must retain the above
+// |   copyright notice, this list of conditions and the
+// |   following disclaimer.
+// |
+// | * Redistributions in binary form must reproduce the above
+// |   copyright notice, this list of conditions and the
+// |   following disclaimer in the documentation and/or other
+// |   materials provided with the distribution.
+// |
+// | * Neither the name of the ray team, nor the names of its
+// |   contributors may be used to endorse or promote products
+// |   derived from this software without specific prior
+// |   written permission of the ray team.
+// |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// +----------------------------------------------------------------------
+#ifndef _H_GAME_OBJECT_MANAGER_H_
+#define _H_GAME_OBJECT_MANAGER_H_
+
+#include <ray/game_features.h>
+
+_NAME_BEGIN
+
+class EXPORT GameObjectManager final : public GameFeature
+{
+	__DeclareSubClass(GameObjectManager, GameFeature)
+	__DeclareSingleton(GameObjectManager)
+public:
+    GameObjectManager() noexcept;
+    ~GameObjectManager() noexcept;
+
+	GameObjectPtr findObject(const std::string& name) noexcept;
+	GameObjectPtr findActiveObject(const std::string& name) noexcept;
+
+	bool activeObject(const std::string& name) noexcept;
+
+	void onFrameBegin() noexcept;
+	void onFrame() noexcept;
+	void onFrameEnd() noexcept;
+
+private:
+	friend GameObject;
+
+	void _instanceObject(GameObject* entity, std::uint32_t& instanceID) noexcept;
+	void _unsetObject(GameObject* entity) noexcept;
+	void _activeObject(GameObject* entity, bool active) noexcept;
+
+private:
+
+	bool _hasEmptyActors;
+
+    std::vector<std::size_t> _emptyLists;
+	std::vector<GameObject*> _instanceLists;
+	std::vector<GameObject*> _activeActors;
+};
+
+_NAME_END
+
+#endif
---
+++ source/game_object_manager.cpp
@@ -0,0 +1,197 @@
+// +----------------------------------------------------------------------
+// | Project : ray.
+// | All rights reserved.
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2015.
+// +----------------------------------------------------------------------
+// | * Redistribution and use of this software in source and binary forms,
+// |   with or without modification, are permitted provided that the following
+// |   conditions are met:
+// |
+// | * Redistributions of source code must retain the above
+// |   copyright notice, this list of conditions and the
+// |   following disclaimer.
+// |
+// | * Redistributions in binary form must reproduce the above
+// |   copyright notice, this list of conditions and the
+// |   following disclaimer in the documentation and/or other
+// |   materials provided with the distribution.
+// |
+// | * Neither the name of the ray team, nor the names of its
+// |   contributors may be used to endorse or promote products
+// |   derived from this software without specific prior
+// |   written permission of the ray team.
+// |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// +----------------------------------------------------------------------
+#include <ray/game_object_manager.h>
+#include <ray/game_object.h>
+
+_NAME_BEGIN
+
+__ImplementSubClass(GameObjectManager, GameFeature, "GameObjectManager")
+__ImplementSingleton(GameObjectManager)
+
+GameObjectManager::GameObjectManager() noexcept
+{
+}
+
+GameObjectManager::~GameObjectManager() noexcept
+{
+}
+
+void
+GameObjectManager::_instanceObject(GameObject* entity, std::uint32_t& instanceID) noexcept
+{
+	if (_emptyLists.empty())
+		_instanceLists.push_back(entity);
+	else
+	{
+		std::size_t _instanceID = _emptyLists.back();
+		_emptyLists.pop_back();
+		_instanceLists[_instanceID - 1] = entity;
+	}
+
+	instanceID = _instanceLists.size();
+}
+
+void 
+GameObjectManager::_unsetObject(GameObject* entity) noexcept
+{
+	auto instanceID = entity->getInstanceID();
+	_instanceLists[instanceID - 1] = nullptr;
+	_emptyLists.push_back(instanceID);
+}
+
+void 
+GameObjectManager::_activeObject(GameObject* entity, bool active) noexcept
+{
+	if (active)
+	{
+		_activeActors.push_back(entity);
+	}
+	else
+	{
+		std::size_t size = _activeActors.size();
+		for (std::size_t i = 0; i < size; i++)
+		{
+			if (_activeActors[i] == entity)
+			{
+				_activeActors[i] = nullptr;
+				_hasEmptyActors = true;
+				break;
+			}
+		}
+	}
+}
+
+GameObjectPtr
+GameObjectManager::findObject(const std::string& name) noexcept
+{
+	for (auto& it : _instanceLists)
+	{
+		if (it)
+		{
+			if (it->getName() == name)
+			{
+				return std::dynamic_pointer_cast<GameObject>(it->shared_from_this());
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+GameObjectPtr 
+GameObjectManager::findActiveObject(const std::string& name) noexcept
+{
+	for (auto& it : _activeActors)
+	{
+		if (it)
+		{
+			if (it->getName() == name)
+			{
+				return std::dynamic_pointer_cast<GameObject>(it->shared_from_this());
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+bool 
+GameObjectManager::activeObject(const std::string& name) noexcept
+{
+	for (auto& it : _instanceLists)
+	{
+		if (it)
+		{
+			if (it->getName() == name)
+			{
+				it->setActive(true);
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void 
+GameObjectManager::onFrameBegin() noexcept
+{
+	for (std::size_t i = 0; i < _activeActors.size(); i++)
+	{
+		if (_activeActors[i])
+			_activeActors[i]->_onFrameBegin();
+	}
+}
+
+void 
+GameObjectManager::onFrame() noexcept
+{
+	for (std::size_t i = 0; i < _activeActors.size(); i++)
+	{
+		if (_activeActors[i])
+			_activeActors[i]->_onFrame();
+	}
+}
+
+void 
+GameObjectManager::onFrameEnd() noexcept
+{
+	for (std::size_t i = 0; i < _activeActors.size(); i++)
+	{
+		if (_activeActors[i])
+			_activeActors[i]->_onFrameEnd();
+	}
+
+	if (_hasEmptyActors)
+	{
+		auto it = _activeActors.begin();
+		auto end = _activeActors.end();
+
+		for (; it != end; ++it)
+		{
+			if (!(*it))
+			{
+				it = _activeActors.erase(it);
+				end = _activeActors.end();
+			}
+		}
+
+		_hasEmptyActors = false;
+	}
+}
+
+_NAME_END
